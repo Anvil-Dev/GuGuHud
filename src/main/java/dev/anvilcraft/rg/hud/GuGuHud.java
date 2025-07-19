1633116b148f08e5f9961b74c37fde44b830f1e2
Gugle <2308465862@qq.com>
2025-07-20 07:42:41 +0800

    修改方法引用，避免直接调用 HudRowManager

--- src/main/java/dev/anvilcraft/rg/hud/GuGuHud.java
+++ src/main/java/dev/anvilcraft/rg/hud/GuGuHud.java
@@ -1,21 +1,27 @@
 package dev.anvilcraft.rg.hud;
 
 import com.mojang.logging.LogUtils;
-import dev.anvilcraft.rg.hud.row.HudRowManager;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
+
+import java.lang.reflect.Method;
 
 @Mod(GuGuHud.MODID)
 public class GuGuHud {
     public static final String MODID = "gugu_hud";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public GuGuHud(IEventBus modEventBus, ModContainer modContainer) {
-        HudRowManager.HUD_ROW_DEFERRED_REGISTER.register(modEventBus);
+    public GuGuHud(IEventBus modEventBus, ModContainer modContainer) throws Exception {
+        if (FMLLoader.getDist() != Dist.CLIENT) return;
+        Class<?> client = GuGuHud.class.getClassLoader().loadClass("dev.anvilcraft.rg.hud.row.HudRowManager");
+        Method method = client.getMethod("register", IEventBus.class);
+        method.invoke(null, modEventBus);
     }
 
     public static @NotNull ResourceLocation of(String path) {
